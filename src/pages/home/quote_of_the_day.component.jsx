c3c1dc99926225d4e7652a58b5350dbd75d4a415
Efen Stakes <efenstakes101@gmail.com>
2022-01-30 18:08:00 +0300

    add a component to show quote of the day

---
+++ src/pages/home/quote_of_the_day.component.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+
+
+import './quote_of_the_day.component.scss'
+
+
+const Quote = require('inspirational-quotes')
+
+
+
+
+const QuoteOfTheDayComponent = () => {
+    const [quote, setQuote] = useState()
+
+    // https://type.fit/api/quotes
+    const getApiQuote = async ()=> {
+        try {
+            let request = await fetch('https://type.fit/api/quotes')
+            let json = await request.json()
+            // console.log('json ', json)
+            // console.log('json length ', json.length)
+
+            const random_index = parseInt(Math.random() * (json.length - 4))
+
+            const random_quote = json[random_index]
+
+            setQuote(random_quote.text)
+        } catch (e) {
+            console.log('error ', e)
+            setQuote(Quote.getRandomQuote())
+        }
+    }
+
+    // console.log(Quote.getQuote({ author: false }))
+    // console.log(Quote.getRandomQuote())
+
+
+    useEffect(()=> {
+        getApiQuote()
+    }, [])
+
+
+    if( !quote ) {
+        return <div />
+    }
+    return (
+        <div className='quote_container'>
+            <p>
+                <q>
+                    {quote}
+                </q>
+            </p>
+        </div>
+    )
+}
+
+
+export default QuoteOfTheDayComponent
